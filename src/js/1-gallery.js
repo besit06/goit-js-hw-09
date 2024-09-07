d5aac2dce9f5dfa881a32277f0d4a3592967a68c
besit06 <behemoth2054@gmail.com>
2024-09-07 11:03:06 +0200

    js09

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -90,10 +90,9 @@ import SimpleLightbox from "simplelightbox";
 
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-new SimpleLightbox('.gallery a', { captionsData: 'alt', 
+new SimpleLightbox('.gallery a',
+  { captionsData: 'alt', 
   captionDelay: 250,  });
 
-const slider = gallery.simpleLightbox(images);
 
-slider.next();
 
